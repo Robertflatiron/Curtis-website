95f3e1fe46dd8a6fbc73cd08cc2cfbb0015757a6
Robert Gaspar <gaspar.robert7@gmail.com>
2022-09-15 13:56:25 -0500

    final completion before customer changes

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
+const services = document.querySelector('.services')
+const exploreButton = document.querySelector('.explore_btn')
+const contact = document.querySelector('.contact')
+const callEmailButton = document.querySelector('.about_btn')
 
 // Display mobile menu
 const mobileMenu = () => {
@@ -22,30 +26,60 @@ const highlightMenu = () => {
 
     //adds 'highlight' class to my menu items
 
-    if(window.innerWidth > 960 && scrollPos < 600) {
+    if(window.innerWidth > 1300 && scrollPos < 600) {
         homeMenu.classList.add('highlight')
         aboutMenu.classList.remove('highlight')
         return
-    } else if (window.innerWidth > 960 && scrollPos < 1400) {
+    } else if (window.innerWidth > 1300 && scrollPos < 1400) {
         aboutMenu.classList.add('highlight')
         homeMenu.classList.remove('highlight')
         servicesMenu.classList.remove('highlight')
         return
-    } else if (window.innerWidth > 960 && scrollPos < 2500) {
+    } else if (window.innerWidth > 1300 && scrollPos < 2000) {
         servicesMenu.classList.add('highlight')
         aboutMenu.classList.remove('highlight')
         contactMenu.classList.remove('highlight')
         return
-    } else if (window.innerWidth > 960 && scrollPos > 2500) {
+    } else if (window.innerWidth > 1300 && scrollPos > 2000) {
         contactMenu.classList.add('highlight')
         servicesMenu.classList.remove('highlight')
         return
     }
 
-    if((bar && window.innerWidth < 960 && scrollPos < 600) || bar) {
+    if((bar && window.innerWidth < 1300 && scrollPos < 600) || bar) {
         bar.classList.remove('highlight')
     }
 }
 
 window.addEventListener('scroll', highlightMenu)
 window.addEventListener('click', highlightMenu)
+
+// close mobile menu when clicking on a menu icon
+
+const hideMobileMenu = () => {
+    const menuBars = document.querySelector('.is-active')
+    if(window.innerWidth <= 768 && menuBars) {
+        menu.classList.toggle('is-active')
+        menuLinks.classList.remove('active')
+    }
+}
+
+menuLinks.addEventListener('click', hideMobileMenu)
+navLogo.addEventListener('click', hideMobileMenu)
+
+// Explore button navigates customer to services-page
+
+const exploreButtonClick = () => {
+    // console.log('clicked')
+    exploreButton.services__wrapper
+}
+exploreButtonClick.addEventListener('click', exploreButton)
+
+//Call or Email button navigates customer to contact page
+
+const callEmailButtonClick = () => {
+    callEmailButton.contact
+}
+callEmailButtonClick.addEventListener('click', callEmailButton)
+
+
